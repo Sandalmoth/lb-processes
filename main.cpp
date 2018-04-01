9fdfeed6de3950054da1c033ff6041b0869dc079
Jonathan Lindström <jonathan.lindstrom@lnu.se>
2018-04-01 20:40:23 +0200

    fixed one issue (uninitialized rates)

--- main.cpp
+++ main.cpp
@@ -12,6 +12,13 @@ using namespace std;
 
 
 int main(int argc, char **argv) {
+
+  mutation_rate = 1e-3;
+  birth_rates[0] = birth_rates[1] = 1.0;
+  death_rates[0] = death_rates[1] = 0.1;
+  birth_interactions[0] = birth_interactions[1] = 0.0;
+  death_interactions[0] = death_interactions[1] = 0.00001;
+
   auto record = make_shared<RecordForest>();
   LB<Cell> lb(record);
   lb.set_cell_count(0, 10);
